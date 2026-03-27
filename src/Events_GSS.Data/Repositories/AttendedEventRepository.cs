1b8eb2ad777bdf14cd5565bc7d445e9e909d1132
berintanemanuel <natiberintan3@gmail.com>
2026-03-28 00:56:24 +0200

    Add AttendedEvent repository interface and basic repository class without implementation.

---
+++ src/Events_GSS.Data/Repositories/AttendedEventRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Events_GSS.Data.Models;
+using Events_GSS.Data.Repositories;
+
+namespace Events_GSS.Data.Repositories
+{
+    public class AttendedEventRepository : IAttendedEventRepository
+    {
+        // sqlConnectionFactory
+        public async Task AddAsync(AttendedEvent attendedEvent)
+        {
+
+        }
+
+        public async Task DeleteAsync(int eventId, int userId)
+        {
+
+        }
+
+        public async Task UpdateIsArchivedAsync(int eventId, int userId, bool isArchived)
+        {
+
+        }
+        public async Task UpdateIsFavouriteAsync(int eventId, int userId, bool isFavourite)
+        {
+
+        }
+        public async Task<AttendedEvent> GetAsync(int eventId, int userId)
+        {
+            return null;
+        }
+        public async Task<List<AttendedEvent>> GetByUserIdAsync(int userId)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Events_GSS.Data/Repositories/IAttendedEventRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Events_GSS.Data.Models;
+
+namespace Events_GSS.Data.Repositories
+{
+    public interface IAttendedEventRepository
+    {
+        Task AddAsync(AttendedEvent attendedEvent);
+        Task DeleteAsync(int eventId, int userId);
+        Task UpdateIsArchivedAsync(int eventId, int userId, bool isArchived);
+        Task UpdateIsFavouriteAsync(int eventId, int userId, bool isFavourite);
+        Task<AttendedEvent> GetAsync(int eventId, int userId);
+        Task<List<AttendedEvent>> GetByUserIdAsync(int userId);
+    }
+}
