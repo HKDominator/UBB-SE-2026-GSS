fc2acdf1519c9fd07ba274d3e5f6a09a174b1736
Andrei-Darius Bodrogean <bodrogean.andrei@gmail.com>
2026-03-27 11:26:29 +0200

    added the announcement model, the announcementraction model and also the IAnnouncementRepository, i also created the Interfaces for the repository folder

---
+++ src/Events_GSS.Data/Models/Announcement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Identity.Client;
+
+namespace Events_GSS.Data.Models
+{
+    public class Announcement
+    {
+        //private int _id;
+        //private string _message;
+        //private DateTime _date;
+        //private bool _isPinned;
+        //private bool _isEdited; no need for these fields as they are already implemented as properties with getters and setters
+        //private Event _event;
+        //private User _author;
+
+        public Announcement( int id, string message, DateTime date )
+        {
+            Id = id;
+            Message = message;
+            Date = date;
+            IsPinned = false;
+            IsEdited = false;
+            IsRead = false;
+            IsExpanded = false;
+
+        }
+
+        public int Id { get; set; }
+        public required string Message { get; set; }
+        public DateTime Date { get; set; }
+
+        public bool IsPinned { get; set; }
+
+        public bool IsEdited { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public bool IsExpanded { get; set; }
+
+        // public Event Event { get; set; }
+
+        // public User Author { get; set; }
+
+    }
+}
---
+++ src/Events_GSS.Data/Models/AnnouncementReaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Identity.Client;
+
+namespace Events_GSS.Data.Models;
+
+public class AnnouncementReaction
+
+{
+    public int Id { get; set; }
+    public required string Emoji { get; set; }
+    public required Announcement Announcement { get; set; }
+
+    public AnnouncementReaction(int id, string emoji, Announcement announcement)
+    {
+        Id = id;
+        Emoji = emoji;
+        Announcement = announcement;
+    }
+
+    //public User Author {  get; set; }
+}
