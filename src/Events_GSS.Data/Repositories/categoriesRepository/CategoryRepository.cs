c49a9a68ee93d7ec7cd40c6f0b3500fffda3565d
amalia-antici <amaliaantici320@gmail.com>
2026-03-28 14:29:53 +0200

    Connection updated for sql

--- src/Events_GSS.Data/Repositories/categoriesRepository/CategoryRepository.cs
+++ src/Events_GSS.Data/Repositories/categoriesRepository/CategoryRepository.cs
@@ -1,24 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 using Events_GSS.Data.Models;
+using Events_GSS.Data.Database;
 
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace Events_GSS.Data.Repositories.categoriesRepository;
 
-internal class CategoryRepository: ICategoryRepository
+public class CategoryRepository : ICategoryRepository
 {
+    private readonly SqlConnectionFactory _connectionFactory;
+
+    public CategoryRepository(SqlConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
     public async Task<List<Category>> GetAllAsync()
     {
-        using var db = Database.DatabaseConnection.CreateContext();
-        return await db.Categories.ToListAsync();
+        var categories = new List<Category>();
+        using var conn = _connectionFactory.CreateConnection();
+        await conn.OpenAsync();
+
+        var cmd = new SqlCommand("SELECT CategoryId, Title FROM Categories", conn);
+        using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            categories.Add(new Category
+            {
+                CategoryId = reader.GetInt32("CategoryId"),
+                Title = reader.GetString("Title")
+            });
+        }
+
+        return categories;
     }
 
     public async Task<Category?> GetByIdAsync(int categoryId)
     {
-        using var db = Database.DatabaseConnection.CreateContext();
-        return await db.Categories.FirstOrDefaultAsync(c => c.CategoryId == categoryId);
+        using var conn = _connectionFactory.CreateConnection();
+        await conn.OpenAsync();
+
+        var cmd = new SqlCommand("SELECT CategoryId, Title FROM Categories WHERE CategoryId = @CategoryId", conn);
+        cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+
+        using var reader = await cmd.ExecuteReaderAsync();
+        if (await reader.ReadAsync())
+        {
+            return new Category
+            {
+                CategoryId = reader.GetInt32("CategoryId"),
+                Title = reader.GetString("Title")
+            };
+        }
+
+        return null;
     }
 }
