3bc2212506a78454eb8edf02a11ec526d3fa6811
berintanemanuel <natiberintan3@gmail.com>
2026-03-31 10:28:58 +0300

    Create interface for mock user service

---
+++ src/Events_GSS.Data/Services/userServices/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Events_GSS.Data.Models;
+
+namespace Events_GSS.Services.Interfaces
+{
+    public interface IUserService
+    {
+        User GetCurrentUser();
+        User? GetUserById(int userId);
+        List<User> GetFriends(int userId);
+        List<User> SearchFriends(int userId, string name);
+    }
+}
