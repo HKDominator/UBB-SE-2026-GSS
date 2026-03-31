fb5102cc9da196936e999d29580aa6c32470208b
berintanemanuel <natiberintan3@gmail.com>
2026-03-31 11:19:24 +0300

    Add back Event and User models. I don't understand why they were deleted.

---
+++ src/Events_GSS.Data/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Events_GSS.Data.Models;
+
+public class Event
+{
+    public int EventId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Location { get; set; } = null!;
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public bool IsPublic { get; set; }
+    public string? Description { get; set; }
+    public int? MaximumPeople { get; set; }
+    public string? EventBannerPath { get; set; }
+    public Category? Category { get; set; }
+    public User Admin { get; set; }
+    public int? SlowModeSeconds { get; set; }
+
+    public int EnrolledCount { get; set; } = 0;
+}
