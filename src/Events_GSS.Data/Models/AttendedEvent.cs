bac507a6289ff1aa5282172ecbaa674b1db0b611
berintanemanuel <natiberintan3@gmail.com>
2026-03-31 16:31:38 +0300

    Add model AttendedEvent entity

---
+++ src/Events_GSS.Data/Models/AttendedEvent.cs
@@ -0,0 +1,21 @@
+﻿using Events_GSS.Data.Models;
+
+public class AttendedEvent
+{
+    public Event Event { get; set; }
+    public User User { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public Boolean IsArchived { get; set; }
+    public Boolean IsFavourite { get; set; }
+
+    public AttendedEvent(Event @event, User user, DateTime enrollmentDate, bool isArchived, bool isFavourite)
+    {
+        Event = @event;
+        User = user;
+        EnrollmentDate = enrollmentDate;
+        IsArchived = isArchived;
+        IsFavourite = isFavourite;
+    }
+
+    public AttendedEvent() { }
+}
