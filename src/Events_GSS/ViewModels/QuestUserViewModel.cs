a86f6f39eb42baf6f7a191f64b2a5b16f9a74086
xaleamo <xaleamo@gmail.com>
2026-03-31 22:30:54 +0300

    Added class QuestUserViewModel

---
+++ src/Events_GSS/ViewModels/QuestUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Events_GSS.Data.Models;
+using Events_GSS.Data.Services.Interfaces;
+
+namespace Events_GSS.ViewModels;
+
+public class QuestUserViewModel
+{
+    private readonly IQuestApprovalService _approvalService;
+    private readonly Event _event;
+    
+    public QuestUserViewModel(IQuestApprovalService approvalService)
+    {
+        _approvalService = approvalService;
+    }
+
+
+
+}
