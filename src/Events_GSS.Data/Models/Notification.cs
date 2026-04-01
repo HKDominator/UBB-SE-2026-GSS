8a73d201fe714625965fa8a3c6dfc79de1e85d41
berintanemanuel <natiberintan3@gmail.com>
2026-04-01 23:47:49 +0300

    Add notification entity model

---
+++ src/Events_GSS.Data/Models/Notification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events_GSS.Data.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Notification(int id, User user, string title, string description)
+        {
+            Id = id;
+            User = user;
+            Title = title;
+            Description = description;
+        }
+
+        public Notification() { }
+    }
+}
