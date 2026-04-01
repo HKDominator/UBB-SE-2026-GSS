87306377e2f23c55551f05f5f72567d3647045cd
berintanemanuel <natiberintan3@gmail.com>
2026-04-02 00:11:36 +0300

    Create the notification repository class without implementation.

---
+++ src/Events_GSS.Data/Repositories/notificationRepository/NotificationRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Events_GSS.Data.Models;
+
+namespace Events_GSS.Data.Repositories.notificationRepository
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        public async Task AddAsync(Notification notification)
+        {
+
+        }
+        public async Task<List<Notification>> GetByUserIdAsync(int userId)
+        {
+            return null;
+        }
+
+        public async Task DeleteAsync(int notificationId)
+        {
+
+        }
+    }
+}
