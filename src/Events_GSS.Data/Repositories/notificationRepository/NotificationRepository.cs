0c18b6e9eb3c21b83e0a9bc1e88fca92cf817baa
berintanemanuel <natiberintan3@gmail.com>
2026-04-02 00:18:50 +0300

    Implement add method

--- src/Events_GSS.Data/Repositories/notificationRepository/NotificationRepository.cs
+++ src/Events_GSS.Data/Repositories/notificationRepository/NotificationRepository.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Events_GSS.Data.Database;
 using Events_GSS.Data.Models;
 
+using Microsoft.Data.SqlClient;
+
 namespace Events_GSS.Data.Repositories.notificationRepository
 {
     public class NotificationRepository : INotificationRepository
@@ -18,6 +20,21 @@ namespace Events_GSS.Data.Repositories.notificationRepository
 
         public async Task AddAsync(Notification notification)
         {
+            const string query = @"
+                INSERT INTO Notifications (Id, UserId, Title, Description, CreatedAt)
+                VALUES (@Id, @UserId, @Title, @Description, @CreatedAt)";
+
+            using var connection = _factory.CreateConnection();
+            await connection.OpenAsync();
+
+            using var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Id", notification.Id);
+            command.Parameters.AddWithValue("@UserId", notification.User.UserId);
+            command.Parameters.AddWithValue("@Title", notification.Title);
+            command.Parameters.AddWithValue("@Description", notification.Description);
+            command.Parameters.AddWithValue("@CreatedAt", notification.CreatedAt);
+
+            await command.ExecuteNonQueryAsync();
 
         }
         public async Task<List<Notification>> GetByUserIdAsync(int userId)
