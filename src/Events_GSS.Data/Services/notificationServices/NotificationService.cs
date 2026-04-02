7eb30c12e95e8306451d70a16a778ffe0802e4e3
berintanemanuel <natiberintan3@gmail.com>
2026-04-02 08:14:53 +0300

    Create notification service class without implementing the methods

---
+++ src/Events_GSS.Data/Services/notificationServices/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Events_GSS.Data.Models;
+using Events_GSS.Data.Repositories.notificationRepository;
+
+namespace Events_GSS.Data.Services.notificationServices
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotificationService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
+        public async Task NotifyAsync(int userId, string title, string description)
+        {
+
+        }
+        public async Task<List<Notification>> GetNotificationsAsync(int userId)
+        {
+            return null;
+        }
+        
+        public async Task DeleteAsync(int notificationId)
+        {
+
+        }
+    }
+}
