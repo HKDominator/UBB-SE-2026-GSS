a1b44d77adfd35c32c2abd0f5b70d6d087e170b8
berintanemanuel <natiberintan3@gmail.com>
2026-04-02 14:17:05 +0300

    Create View model

---
+++ src/Events_GSS/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Events_GSS.Data.Services.notificationServices;
+using Events_GSS.Services.Interfaces;
+
+namespace Events_GSS.ViewModels
+{
+    public class NotificationViewModel
+    {
+        private readonly INotificationService _notificationService;
+        private readonly IUserService _userService;
+
+
+        public NotificationViewModel(INotificationService notificationService, IUserService userService)
+        {
+            _userService = userService;
+            _notificationService = notificationService;
+        }
+    }
+}
